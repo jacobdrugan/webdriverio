ec7f11c6a6ab048bfcc38565438880348f42845c
Jacob Drugan <jacobdrugan@gmail.com>
2022-09-20 14:15:12 +0000

    Betslip.js now functional

--- test/step-definitions/betslip.steps.ts
+++ test/step-definitions/betslip.steps.ts
@@ -1,4 +1,4 @@
-import { Given,When,Then } from "@wdio/cucumber-framework"
+import { Given,When,Then, BeforeStep } from "@wdio/cucumber-framework"
 import Page from "../pageobjects/page"
 import SportsPage from '../pageobjects/sports.page'
 const pages = {
@@ -10,14 +10,27 @@ Given(/^I am on the (\w+) page$/, async (page: keyof typeof pages) => {
     await Page.acceptOneTrust()
 });
 
-When(/^I add (\d+) bets to the betlip$/, async (numberofbets) => {
-    expect (await Page.btnLogin).toBeDisplayed()
-    //Blocked here, cannot get assertion to work and therefore is blocking clicks from happening
+When(/^I add (\d+) bet to the betlip$/, async (numberofbets) => {
+    await SportsPage.mainContent.waitForDisplayed()
     await SportsPage.addNumberofBets(numberofbets)
 });
 
 Then(/^I can see (\d+) in the count of bets$/, async (numberofbets) => {
-    console.log(numberofbets)
-    console.log(SportsPage.countofbetsonslip)
-    expect(SportsPage.countofbetsonslip).toEqual(numberofbets)
+    expect(SportsPage.listBetslipcount).toBeElementsArrayOfSize(numberofbets)
 });
+
+/*
+Given(/^I am on the (\w+) page$/, async (page: keyof typeof pages) => {
+    await pages[page].open()
+    await Page.acceptOneTrust()
+});
+
+When(/^I add (\d+) bets to the betlip$/, async (numberofbets) => {
+    await SportsPage.mainContent.waitForDisplayed()
+    await SportsPage.addNumberofBets(numberofbets)
+});
+
+Then(/^I can see (\d+) in the count of bets$/, async (numberofbets) => {
+    expect(SportsPage.listBetslipcount).toBeElementsArrayOfSize(numberofbets)
+});
+*/
